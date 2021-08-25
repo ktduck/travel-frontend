b24d720566159f6007ec186584d6b817d443f9df
kongming0917 <jdj0917@gmail.com>
2021-08-25 21:03:36 +0900

    Add test file [test]

---
+++ test/JS/5.js
@@ -0,0 +1,34 @@
+const phoneNum = document.getElementById("answer");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const del = document.getElementById("del");
+
+function clickNum(event){
+    event.preventDefault();
+    phoneNum.value += event.path[0].innerText;
+}
+
+function delNum(event){
+    event.preventDefault();
+    phoneNum.value = phoneNum.value.slice(0,-1);    
+}
+
+
+
+one.addEventListener("click", clickNum);
+two.addEventListener("click", clickNum);
+three.addEventListener("click", clickNum);
+four.addEventListener("click", clickNum);
+five.addEventListener("click", clickNum);
+six.addEventListener("click", clickNum);
+seven.addEventListener("click", clickNum);
+eight.addEventListener("click", clickNum);
+nine.addEventListener("click", clickNum);
+del.addEventListener("click", delNum);
